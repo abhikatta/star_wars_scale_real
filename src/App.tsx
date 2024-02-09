e5913a3ae13c3684e91b38c843682c9958240fb4
unknown <abhinaykatta97@gmail.com>
2024-02-09 18:01:32 +0530

    setup and initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" h-screen w-screen flex flex-col items-center justify-center bg-slate-700 text-slate-100">
+      <p>App</p>
     </div>
   );
-}
+};
 
 export default App;
